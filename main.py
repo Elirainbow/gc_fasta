69899146f063496fec5c40a81c94728abad80e5a
Elirainbow <elizabeth12.jimenez07@gmail.com>
2026-05-16 22:55:05 -0600

    feat: leer archivo fasta y calcular el porcentage de GC

---
+++ main.py
@@ -0,0 +1,77 @@
+def leer_fasta(ruta_archivo):
+    """Lee un archivo FASTA y concatena todas las secuencias de DNA.
+
+    Esta función lee un archivo en formato FASTA, ignorando las líneas de
+    encabezado (que comienzan con '>'), y concatena todas las líneas de
+    secuencia en un único string.
+
+    Args:
+        ruta_archivo (str): Ruta del archivo FASTA a leer.
+
+    Returns:
+        str: String con todas las secuencias de DNA concatenadas.
+    """
+    secuencia = ""
+
+    with open(ruta_archivo, "r", encoding="utf-8") as archivo:
+        for linea in archivo:
+            linea = linea.strip()
+
+            if not linea.startswith(">"):
+                secuencia += linea  # La variable `secuencia` es un string, es decir, una cadena de texto. Estaamos concatenando strings.
+    return secuencia
+
+
+secuencia = leer_fasta("secuencia.fasta")
+# print(secuencia)
+
+
+def calcular_gc(secuencia):
+    """Calcula el porcentaje de bases G y C en una secuencia de DNA.
+
+    Cuenta el número de bases G (guanina) y C (citosina) en la secuencia
+    y calcula qué porcentaje representan del total de nucleótidos.
+    Esta es una métrica importante para caracterizar secuencias de DNA.
+
+    Args:
+        secuencia (str): variable que contiene la secuencia de DNA
+        (esperada en mayúsculas) concatenada en la función leer_fasta.
+
+    Returns:
+        float: Porcentaje de GC en la secuencia (0-100).
+    """
+
+    cantidad_g = secuencia.count("G")
+    cantidad_c = secuencia.count("C")
+
+    gc = cantidad_c + cantidad_g
+    longitud = len(secuencia)
+    porcentaje_gc = gc / longitud * 100
+    return porcentaje_gc
+
+
+# resultado = calcular_gc("GGGGTTTT")
+# print(resultado)
+
+
+def main():
+    """Función principal que integra la lectura FASTA y el cálculo de GC.
+
+    Lee una secuencia de DNA desde un archivo FASTA y calcula el porcentaje
+    de contenido GC, mostrando el resultado en la consola con dos decimales
+    de precisión.
+
+    Returns:
+        None: Solo imprime el resultado en consola.
+    """
+    ruta_archivo = "secuencia.fasta"
+    secuencia = leer_fasta(ruta_archivo)
+    contenido_gc = calcular_gc(secuencia)
+
+    print(f"la secuencia tiene: {contenido_gc:.2f}% de GC")
+
+
+if __name__ == "__main__":
+    main()
+
+# > Si este archivo se está ejecutando directamente como programa principal, entonces llama a `main()`.
